875242b16b7b49aeb4c1589187994c7d5f7c5cca
finnlattimore@gmail.com <finnlattimore@gmail.com@7d601506-13fb-0035-ea54-101c9558cf7b>
2013-07-07 01:42:44 +0000

    Initial commit

---
+++ trunk/com.finn.art.cogs/src/CogApp.java
@@ -0,0 +1,309 @@
+import geomerative.RG;
+import geomerative.RPoint;
+import geomerative.RShape;
+
+import java.awt.Color;
+import java.util.List;
+
+import processing.core.PApplet;
+
+
+
+public class CogApp extends PApplet {
+	GearShape gear1; 
+	ConjugateGear conjugate; 
+	GearShape gear2;
+	boolean record;
+	
+	List<Float> radii;
+	List<RPoint> points;
+	List<Float> angles;
+	int numAngles;
+	RPoint origin = new RPoint(0,0);
+	RPoint prevPoint = null;
+	RPoint prevCutterOrigin = new RPoint(0,0);
+	float cutterAngle = 0;
+	float cutterRadius = 0f;
+	Cutter cutter;
+	RShape cutShape;
+	int numCutterAngles = 1000;
+	int loop = 0;
+	RPoint xAxis = new RPoint(1,0);
+	
+	
+
+	@Override
+	public void setup() {	
+		size(1000,1000, P3D);
+		RG.init(this);
+		gear1 = new GearShape(this);
+		gear1.setSinusoidalProfile(100, 50, 2, 1000);
+		
+		radii = gear1.getRadii();
+		points = gear1.getPoints();
+		numAngles = radii.size();
+		cutter = new Cutter(gear1.getShape().getCurveLength(), 10, 3, 5, 100, this);
+		cutShape = cutter.getShape();
+		cutterRadius = cutter.getRadius();
+		angles = gear1.getAngles();
+		//frameRate(5);
+	}
+	
+	public void draw4(){
+		background(Color.WHITE.getRGB());
+		translate(width/2,height/2);
+		RG.shape(cutShape);
+	}
+	
+	
+	public void draw() {
+		background(Color.WHITE.getRGB());
+		translate(width/2,height/2);
+	
+		RPoint p = points.get(loop);
+		RPoint norm = gear1.getNorms().get(loop);
+		RPoint scaledNorm = new RPoint(norm);
+		scaledNorm.scale(cutterRadius);
+
+		//line(0,0,p.x,p.y);
+		
+		//translate the cutter.
+		RPoint cutterOrigin = new RPoint(p);
+		cutterOrigin.add(scaledNorm);
+		RPoint trans = new RPoint(cutterOrigin);
+		trans.sub(prevCutterOrigin);
+		cutShape.translate(trans);
+		
+		line(0,0,cutterOrigin.x,cutterOrigin.y);
+
+		if (prevPoint != null) {
+			float dist = p.dist(prevPoint);
+			cutterAngle += dist/(float)cutterRadius;
+			//actually rotate the cutter here ...	
+			cutShape.rotate(dist/(float)cutterRadius,cutterOrigin);
+		}
+
+		for (int j = 0; j < numCutterAngles; j ++) {
+			float adv = j/(float)numCutterAngles;
+			RPoint cutterPoint = cutShape.getPoint(adv);
+			float angleFromOrigin = atan2(cutterPoint.y,cutterPoint.x);
+			if (angleFromOrigin < 0) {
+				angleFromOrigin += 2*PI;
+			}
+			
+			float distFromOrigin = cutterPoint.norm();
+			int angleIndx = gear1.getRadialIndx(angleFromOrigin);
+			radii.set(angleIndx, Math.min(radii.get(angleIndx), distFromOrigin));
+		}
+
+		gear1.draw(0, 0);
+		//cutter.draw();
+		RG.shape(cutShape);
+		//ellipse(cutterOrigin.0, cutter)
+
+		prevPoint = p;
+		prevCutterOrigin = cutterOrigin;
+		loop ++;
+		
+		if (loop > numAngles) {
+			noLoop();
+		}
+		
+		
+		
+	}
+	
+	
+//	public void draw3() {
+//		
+//		 if (record) {
+//			    beginRaw(DXF, "/home/finn/programming/hacking/non_circ_gears/output.dxf");
+//		 }
+//		background(Color.WHITE.getRGB());
+//		translate(width/2,height/2);
+//		
+//		distAlongCurve = currentPoint/(float)npoints;
+//		RPoint p = gear1.getShape().getPoint(distAlongCurve);
+//		ds = p.dist(pLast);
+//		RPoint t = gear1.getShape().getTangent(distAlongCurve);
+//		
+//		RPoint norm = Vector.getUnitNorm(t);
+//		norm.scale(r);
+//		
+//		//shift the cutter origin to the new point.
+//		RPoint cutterOriginPrev = cutterOrigin; //this is where the cutter was
+//		
+//		cutterOrigin = new RPoint(p);
+//		cutterOrigin.add(norm); //this is where the cutter is now
+//		
+//		RPoint trans = new RPoint(cutterOrigin);
+//		trans.sub(cutterOriginPrev); //this is the difference
+//		
+//		cutter.translate(trans);
+//		
+//		cutter.rotate(ds/r, cutterOrigin);//
+//		
+//		
+//		gear1.draw(0, 0);
+//		
+//		stroke(Color.RED.getRGB());
+//		RG.shape(cut);
+//		stroke(Color.BLACK.getRGB());
+//		line(0,0,p.x,p.y);
+//		line(p.x,p.y,p.x+norm.x,p.y+norm.y);
+//		line(0,0,cutterOrigin.x,cutterOrigin.y);
+//		//ellipse(cutterOrigin.x,cutterOrigin.y,2*r,2*r);
+//		
+//		cut = subtract(cutter,cut);
+//		RG.shape(cutter);
+//		
+//			
+//		
+//		
+//		currentPoint ++;
+//		pLast = p;
+//			
+//		
+//		 if (record) {
+//			    endRaw();
+//			    record = false;
+//			    System.out.println("DXF written");
+//		 }
+//		 
+//		
+//		//noLoop();
+//	}
+	
+	/*** subtract any bits of shape1 that go into shape2. ***/
+	private static RShape subtract(RShape shape1, RShape shape2) {
+		float smallStep = 0.0001f;
+		float bigStep = 0.01f;
+		
+		RPoint[] intersects = shape2.getIntersections(shape1);
+		RShape result = new RShape();
+		result.addMoveTo(shape2.getPoint(0));
+		
+		float theta = atan(intersects[0].y/intersects[0].x);
+		float startAdv = theta/(2*PI);
+		float theta2 = atan(intersects[1].y/intersects[1].x);
+		float endAdv = theta2/(2*PI);
+		
+		float deltaAdv2 = endAdv - startAdv;
+		
+		if (deltaAdv2 <  smallStep) {
+			
+			return shape2;
+		}
+		
+		float adv1 = findAdv(shape1, intersects[1], 1000);
+		float adv2 = findAdv(shape1, intersects[0], 1000);
+		float deltaAdv1 = adv2 - adv1;
+		
+		//System.out.println(deltaAdv2+","+deltaAdv1);
+		
+
+		float adv = 0;
+		while (adv < startAdv - bigStep ) {
+			RPoint p = shape2.getPoint(adv);
+			result.addLineTo(p);
+			adv += bigStep;
+		}
+		adv = startAdv - bigStep;
+		while (adv < startAdv) {
+			RPoint p = shape2.getPoint(adv);
+			result.addLineTo(p);
+			adv += smallStep;
+		}
+		
+		adv = adv1;
+		while (adv < adv2) {
+			//System.out.println("Adv,"+adv+" start,"+adv1+" end,"+adv2);
+			RPoint p = shape1.getPoint(adv);
+			result.addLineTo(p);
+			adv += smallStep;
+		}
+		
+		adv = endAdv;
+		while (adv < .9999f) {
+			
+			RPoint p = shape2.getPoint(adv);
+			result.addLineTo(p);
+			adv += bigStep;
+		}
+		
+		
+		return result;
+	    
+	}
+	
+	/*** find the advancement that yeilds a point as close as possible to p. ***/
+	private static float findAdv(RShape s, RPoint p, int resolution) {
+		float minDist = Float.MAX_VALUE;
+		float closestAdv = 0f;
+		for (int i = 0;  i < resolution; i ++) {
+			float adv = i/(float)resolution;
+			RPoint shapePoint = s.getPoint(adv);
+			float dist = shapePoint.dist(p);
+			if (dist < minDist) {
+				minDist = dist;
+				closestAdv = adv;
+			}
+		}
+		
+		return closestAdv;
+	}
+	
+	
+	
+	
+	
+	private RShape test(){
+		RShape circle = new RShape();
+		float r = 30;
+		float theta = 0;
+		
+		int points = 100;
+		float dtheta = 2*PI/(float)points;
+		for (int i = 0; i < points; i ++) {
+			circle.addLineTo(r*cos(theta) - 30, r*sin(theta));
+			theta += dtheta;
+		}
+		circle.addClose();
+		RShape t = new RShape();
+		t.addLineTo(-30, -5);
+		t.addLineTo(-30,5);
+		t.addClose();
+		circle.addShape(t);
+		
+		circle.translate(30, 0);
+		return circle;
+	}
+	
+	private RShape circle(float radius, int npoints) {
+		RShape s = new RShape();
+		s.addMoveTo(radius, 0);
+		float theta = 0;
+		float dtheta = 2*PI/(float)npoints;
+		for (int i = 0; i < npoints; i++) {
+			s.addLineTo(radius * cos(theta), radius*sin(theta));
+			theta += dtheta;
+		}
+		s.addClose();
+		return s;
+	}
+	
+	
+	/*** record the data to a dxf file if r is pressed. ***/
+	public void keyPressed() {
+		  if (key == 'r') {
+		  	record = true;
+		  }
+	}
+	
+
+	 public static void main(String args[]) {
+		    PApplet.main(new String[] { "--present", "CogsApplet" });
+	 }
+	
+
+}
---
+++ trunk/com.finn.art.cogs/src/GearShape.java
@@ -0,0 +1,320 @@
+import geomerative.RG;
+import geomerative.RPoint;
+import geomerative.RPolygon;
+import geomerative.RShape;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import processing.core.PApplet;
+import static processing.core.PApplet.pow;
+import static processing.core.PApplet.sqrt;
+import static processing.core.PApplet.cos;
+import static processing.core.PApplet.sin;
+import static processing.core.PApplet.PI;
+
+/*** This class represents a drawable non-linear gear. ***/
+public class GearShape {
+	//private List<Double> radialFunction;
+//	private List<Double> angles;
+	private PApplet app;
+	private float centerRadius = 10;
+	private int angleIndxResolution = 10000;
+	
+	private List<Float> angles;
+	private List<Float> radii;
+	private List<RPoint> norms;
+	private List<RPoint> points; //this duplicates infomation held by radii and angle but makes calculations quicker.
+	private RShape shape; 
+	private int[] angleIndx;
+
+	
+	/*** Create a new gear with the specified radial points at the corresponding angles.
+	 * 
+	 * @param radialFunction the points
+	 * @param angles the angles, must contain the same number of elements as the points
+	 * @param app
+	 */
+	public GearShape(PApplet app) {
+		this.app = app;
+	}
+	
+	/*** set the profile of the gear from a list of radii. The radii are assumed to be at equally spaced angles. ***/
+	public void setProfile(List<Float> radialFunction) {
+		List<Float> angles = Vector.createEvenAngles(radialFunction.size());
+		setProfile(radialFunction,angles);
+	}
+	
+	
+	/*** set the profile of the gear. ***/
+	public void setProfile(List<Float> radialFunction, List<Float> angles) {
+		if (radialFunction.size() != angles.size()) {
+			throw new IllegalArgumentException("number of angles must match number of radii");
+		}
+		//this.radii = radialFunction;
+		//this.angles = angles;
+		this.points = new ArrayList<RPoint>(angles.size());
+		this.norms = new ArrayList<RPoint>(angles.size());
+		this.angles = new ArrayList<Float>(angles.size());
+		this.radii = new ArrayList<Float>(angles.size());
+		this.shape = new RShape();
+		
+		if (radialFunction.size() > 0) {
+			float r = radialFunction.get(0);
+			float theta = angles.get(0);
+			shape.addMoveTo(r*cos(theta), r*sin(theta));
+			for (int i = 1; i < angles.size(); i++) {
+				r = radialFunction.get(i);
+				theta = angles.get(i);
+				shape.addLineTo(r*cos(theta),r*sin(theta));
+			}
+			shape.addClose();
+		}
+		// now using the shape we have created we pre-calculate the points and tangents, radii and angles.
+		// this will also resample the points so that the points are equally distributed by arc-length (NOT angle)
+		int npoints = angles.size();
+		RPoint xAxis = new RPoint(1,0);
+		for (int i = 0; i < npoints; i ++) {
+			float adv = i/(float)npoints;
+			RPoint tangent = shape.getTangent(adv);
+			RPoint norm = Vector.getUnitNorm(tangent);
+			RPoint point = shape.getPoint(adv);
+			//float angle = point.angle(xAxis);
+			float angle = (float) Math.atan2(point.y,point.x);
+			if (angle < 0) {angle += 2*PI;}
+			
+			float radius = point.norm();
+			this.angles.add(angle);
+			radii.add(radius);
+			points.add(point);
+			norms.add(norm);
+		}
+		
+		
+		buildAngleIndx(angleIndxResolution);
+		testEquivelence();
+	}
+	
+	private void buildAngleIndx(int numSteps) {
+		angleIndx = new int[numSteps];
+		int numAngles = angles.size();
+		int j = 0;
+		for (int i = 0; i < numSteps; i ++) {
+			float currentAngle  = angles.get(j);
+			float nextAngle = angles.get((j+1)%numAngles);
+			float ourAngle = 2*PI*i/numSteps;
+			if (nextAngle < currentAngle) {
+				nextAngle += 2*PI;
+			}
+			if (ourAngle > (currentAngle + nextAngle)/2f){
+				j ++;
+			}
+			if (j >= angles.size()) {
+				j = 0;
+			}
+			angleIndx[i] = j;
+		}
+	}
+	
+	public int getRadialIndx(float angle) {
+		return angleIndx[(Math.round(angle*angleIndx.length/(2*PI))+angleIndx.length)%angleIndx.length];
+	}
+	
+	
+	/*** Create a circular gear modified by a sin wave. 
+	 * The radial function is of the form R = A + B*sin(C*t). 
+	 * @A the minimum radius
+	 * @B the variable radius
+	 * @C the frequency
+	 * Primarily for testing purposes. ***/		
+	public void setSinusoidalProfile(float minRadius, float variableRadius, int frequency, int nPoints) {
+		if (variableRadius > minRadius) {throw new IllegalArgumentException("B must be strictly smaller than A, otherwise the radial function will be negative at some points.");}
+		List<Float> points = new ArrayList<Float>(nPoints);
+		List<Float> angles = Vector.createEvenAngles(nPoints);
+		for (int i = 0; i < nPoints; i++) {
+			float theta = angles.get(i);
+			float value = minRadius + variableRadius*sin(frequency*theta);
+			points.add((float) value);
+		}
+		setProfile(points, angles);
+	}
+	
+	
+	/*** Create the gear used as an example in various papers by B. Laczic. Used for testing purposes. ***/
+	public void setLaczikGearProfile(int nPoints, float scale) {
+		List<Float> points = new ArrayList<Float>(nPoints);
+		List<Float> angles = Vector.createEvenAngles(nPoints);
+		for (int i = 0; i < nPoints; i++) {
+			float theta = angles.get(i);
+			float a = 1/7.0f;
+			float b = 1/9.0f;
+			float c = -2/32.0f;
+			float val = a*cos(theta)+2*b*cos(2*theta)+3*c*cos(3*theta);
+			float radius = (scale)*((val + 1)/(val + 2));
+			points.add(radius);
+		}
+		setProfile(points, angles);
+	}
+	
+	public int getNumAngles(){
+		return radii.size();
+	}
+	
+	public List<Float> getRadii() {
+		return this.radii;
+	}
+	
+	public List<RPoint> getNorms() {
+		return this.norms;
+	}
+	
+	public List<RPoint> getPoints(){
+		return this.points;
+	}
+	
+	public List<Float> getAngles(){
+		return angles;
+	}
+	
+
+	
+	
+
+	
+
+	
+	/*** Draw the gear centered at the specified point. ***/
+	public void draw(float x, float y){
+		app.pushMatrix();
+		app.translate(x, y);
+		
+		RG.shape(shape,0,0);
+		app.ellipse(0,0,centerRadius,centerRadius);
+		
+		app.stroke(Color.RED.getRGB());
+		int numAngles = getNumAngles();
+		float r = radii.get(0);
+		float theta = angles.get(0);
+		float xLast = r*cos(theta);
+		float yLast = r*sin(theta);
+		for (int i = 1; i < numAngles; i ++) {
+			r = radii.get(i);
+			theta = angles.get(i);
+			float xnew = r*cos(theta);
+			float ynew = r*sin(theta);
+			app.line(xLast, yLast, xnew, ynew);
+			xLast = xnew;
+			yLast = ynew;
+			
+		}
+		app.stroke(Color.BLACK.getRGB());
+		
+		app.popMatrix();
+	}
+	
+	
+	public float getCenterRadius() {
+		return centerRadius;
+	}
+
+	public void setCenterRadius(float centerRadius) {
+		this.centerRadius = centerRadius;
+	}
+
+	public RShape getShape() {
+		return shape;
+	}
+	
+	/*** This function tests that the data stored in the radii and angle is consistent with the data in the points. ***/
+	private void testEquivelence(){
+		int numAngles = getNumAngles();
+		
+		for (int i = 0; i < numAngles; i ++) {
+			float adv = i/(float)numAngles;
+			RPoint p1 = shape.getPoint(adv);
+			RPoint p2 = points.get(i);
+			float dist = p1.dist(p2); //this should equal 0
+			float r = p2.norm();
+			float angle = (float) Math.atan2(p1.y,p1.x);
+			if (angle < 0) {angle += 2*PI;}
+			float rDiff = r - radii.get(i);
+			float angleDiff = angle - angles.get(i);
+			
+			if (dist != 0f || rDiff != 0f || angleDiff != 0f) {
+				throw new IllegalStateException("Angle and radius data is incositant with points. ");
+			}
+		}
+	}
+	
+//	/*** set the gear to rotate. ***/
+//	public void rotate(double speed){
+//	//speed should be rotations per second	
+//		for (int i = 0; i < angles.size(); i++) {
+//			double current = angles.get(i);
+//			angles.set(i, current+speed);
+//		}
+//	}
+//	
+//	/*** Shifts the angle function so that the minimum radius lies along the x-axis. ***/
+//	public void rotateToAllignMinWithXaxis(){
+//		int indx = radialFunction.indexOf(Collections.min(radialFunction));
+//		double thetaAtMinRadius = angles.get(indx);
+//		for (int i = 0; i < angles.size(); i ++) {
+//			double newAngle = (angles.get(i) - thetaAtMinRadius)%(2*Math.PI);
+//			angles.set(i, newAngle);
+//		}	
+//	}
+//	
+//	/*** Shifts the angle function so the maximum radius lies along the -x-axis. ***/
+//	public void rotateToAllignMaxWithNegXaxis(){
+//		int indx = radialFunction.indexOf(Collections.max(radialFunction));
+//		double thetaAtMaxRadius = angles.get(indx);
+//		for (int i = 0; i < angles.size(); i++) {
+//			double newAngle = (angles.get(i) + Math.PI - thetaAtMaxRadius)%(2*Math.PI);
+//			angles.set(i, newAngle);
+//		}
+//	}
+
+	
+	
+	
+	//int nPoints = 5000;
+	//for (int i = 0; i < nPoints; i ++) {
+		//shape.rotate(2*PI/(float)nPoints);
+	
+				
+	//}
+	
+	//shape = toothedShape;
+	//float dist = i/((float)nPoints);
+//	RPoint p = shape.getPoint(dist);
+//	RPoint t = shape.getTangent(dist); // if we draw a point from x,y to x+t.x,y+t.y its a tangent.
+//	float tscale = toothAmplitude/(sqrt(pow(t.x, 2)+pow(t.y,2)));
+//	float shift = cos(dist*a*2*PI*100);
+//	//we want to shift the point by shift, perpendicular to the tangent line
+//	
+//	float shiftx  = -shift*tscale*t.y;
+//	float shifty = shift*tscale*t.x;
+//	
+//	//app.line(p.x, p.y, p.x - tscale*t.y, p.y + +tscale*t.x);
+//	
+//	p.translate(shiftx, shifty);
+//	toothedShape.addLineTo(p);
+	
+	
+//	
+//	double thetaPrev = angles.get(0);
+//	for (int i = 1; i < angles.size(); i++) {
+//		double theta = angles.get(i);
+//		double dtheta = theta - thetaPrev;
+//		double ds  = radialFunction.get(i) * dtheta;//arc distance we have moved = r*dtheta
+//		// find the tangent line to the gear at this point.
+//		
+//		// RPoint tangent = shape.getTangent(i/numPoints);
+//		
+//		//twist the gear a tiny bit
+//		//adjust the r and theta
+}
