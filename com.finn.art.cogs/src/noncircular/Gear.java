a4242a94aba62b726db148e7416a301f71a64e20
finnlattimore@gmail.com <finnlattimore@gmail.com@7d601506-13fb-0035-ea54-101c9558cf7b>
2013-07-27 04:19:00 +0000

    non-circular2 is now working we just need to add teeth

--- com.finn.art.cogs/src/noncircular/Gear.java
+++ com.finn.art.cogs/src/noncircular/Gear.java
@@ -35,14 +35,30 @@ public class Gear {
 		return result;
 	}
 	
-	public void setProfile(List<Float> radii, List<Float> angles) {
-		setProfile(copyListToArray(radii), copyListToArray(angles));
+	public void setColor(int rgb) {
+		shape.fill(rgb);
+		shape.stroke(rgb);
 	}
 	
-	public void setProfile(float[] radii, float[] angles) {
+	public void setProfile(List<Float> radii, List<Float> angles, boolean reverseAxis) {
+		setProfile(copyListToArray(radii), copyListToArray(angles), reverseAxis);
+	}
+	
+	public void setProfile(float[] radii, float[] angles, boolean reverseAxis) {
 		if (angles.length != radii.length) {
 			throw new IllegalArgumentException("radii array lenght must match angle array length");
 		}
+		if (reverseAxis) { //the angles are specified relative to the -ve x axis.
+			int indx = 0;
+			for (float phi: angles) {
+				phi = PI - phi;
+				if (phi < 0 ) {phi = phi + 2*PI;}
+				angles[indx] = phi;
+				indx ++;
+			}
+		}
+		
+		
 		this.angles = angles;
 		this.radii = radii;
 		shape = app.createShape();
@@ -67,7 +83,7 @@ public class Gear {
 			angles[i] = theta;
 			radii[i] = r;
 		}
-		setProfile(radii, angles);
+		setProfile(radii, angles, false);
 	}
 	
 	private List<Float> copyArrayToList(float[] array) {
@@ -86,11 +102,21 @@ public class Gear {
 		return copyArrayToList(angles);
 	}
 	
+	/*** Adds teeth to this gear. 
+	 * The teeth are added only the what is drawn, the underlying angles and radii of the pitch curve are not changed ***/
+	public void addTeeth(int numTeeth, float toothDepth) {
+		
+	}
 	
-	
+	/*** Expands the shape that is drawn for the gear such that it can cut into the blanks in the other gear.
+	 * Does not modify the pitch curve. 
+	 */
+	public void expand(float depth) {
+		
+	}
 	
 	public void draw() {
-		app.shape(shape);
+		app.shape(shape);		
 	}
 	
 	
